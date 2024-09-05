fb2302fa93b8dd16d75701458c07547621b1df69
Murillo Martins Proveza <murillomartinsproveza@gmail.com>
2024-09-05 15:57:48 -0300

    first commit

---
+++ main.c
@@ -0,0 +1,14 @@
+#include <stdlib.h> // Cabecalho de Biblioteca padrao
+#include <stdio.h> // Cabecalho de Biblioteca de I/O padrao
+#include <sched.h> // Cabecalho de Biblioteca de escalonamento
+#include <sys/types.h> // Cabecalho com definicao de tipos de dados
+#include <sys/utsname.h>// Cabecalho com definicao da estrutura utsname
+#include <unistd.h> // Cabecalho com definicao de constantes padra
+
+int main() {
+
+printf("teste");
+
+
+    return 0;
+} 
